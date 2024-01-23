a8e6434c707b01c027124c0e6de6b3cab712ae4f
Vanshgoel2610 <goelv2610@gmail.com>
2024-01-23 12:46:06 +0530

    Initail commit vansh

---
+++ deleteProfile.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include("dbconnect.php");
+?>
+<?php
+if(isset($_SESSION['uname']) && isset($_SESSION['pass'])) {
+    $un = $_SESSION['uname'];
+    $pd = $_SESSION['pass'];
+
+    // Create a prepared statement
+    $query = "DELETE FROM voter WHERE uname = '$un' AND pass = '$pd' ";
+    if(mysqli_query($con,$query))
+    {
+        header("location:index.html");
+        echo "profile deleted";
+    }
+    else
+    {
+        echo "Error: " . mysqli_error($conn);
+    }
+    exit(0);
+}
+?>
---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+if(!(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] != true)) {
+    echo '<script>
+    var confirmLogout = confirm("Are you sure you want to log out?");
+    if (confirmLogout) {
+        // User clicked "OK" in the confirmation prompt
+        window.location.href = "logout.php"; // Replace with the actual logout URL
+    } else {
+        // User clicked "Cancel" in the confirmation prompt
+        // You can add additional logic here if needed
+    }
+    </script>';
+    
+}
+session_unset();
+session_destroy();
+header("location: vansh_login.html");
+exit;
+?>

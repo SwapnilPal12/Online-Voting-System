29cb9a50be08a1e8badc9c8d2124143a12470eb8
Swapnil Pal <sanjibpal1212@gmail.com>
2024-01-24 14:29:18 +0530

    repo

--- login.php
+++ login.php
@@ -5,6 +5,8 @@ include("dbconnect.php");
 <?php
 $un=$_POST['t1'];
 $pd=$_POST['t3'];
+$special_username = "SwapnilPal";
+$special_password = "swapnil@123";
 $sql="SELECT * FROM voter";
 $result=mysqli_query($conn,$sql);
 $c=0;
@@ -14,12 +16,19 @@ $c=0;
          {
             $c++;
         }
+        else if($un == $special_username && $pd == $special_password) {
+            $c--;
+        }
+
         
     }
     if($c== 0)
     {
         header("location:wrong.html");
     }
+    else if($c<=-1){
+        header("location: displayuser.php");}
+    
     else
     {
         header("location:voting.html");
